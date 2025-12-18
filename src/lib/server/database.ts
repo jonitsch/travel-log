ab0e8c9efb2e79a69aa1b935d43a9633570472af
Jonathan <joni.tschanter@gmx.de>
2025-12-18 01:12:11 +0100

    Fix Prisma Require Active Adapter

--- src/lib/server/database.ts
+++ src/lib/server/database.ts
@@ -1,6 +1,8 @@
+import { env } from "$env/dynamic/private";
 import { PrismaClient } from "$gen/prisma/client/client";
+import { PrismaMariaDb } from "@prisma/adapter-mariadb";
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient({ adapter: new PrismaMariaDb(env.DATABASE_URL)});
 
 export { prisma };
 
