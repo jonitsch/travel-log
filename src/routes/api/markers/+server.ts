35560c1eb9d06478309c6bcc3d8bcb617af03d9e
Jonathan <joni.tschanter@gmx.de>
2025-12-18 21:35:51 +0100

    Update Image and Marker APIs

--- src/routes/api/markers/+server.ts
+++ src/routes/api/markers/+server.ts
@@ -3,12 +3,18 @@ import { json } from '@sveltejs/kit';
 
 export async function GET(journeyId) {
   let journeyIdString = journeyId.url.search.split('=')[1]
-  let markers = await prisma.marker.findMany(
-    {
-      where: {
-        journeyId: journeyIdString
-      }
+  let markers: {
+    name: string;
+    id: number;
+    lng: number;
+    lat: number;
+    journeyId: string;
+    color: string;
+  }[] = await prisma.marker.findMany({
+    where: {
+      journeyId: journeyIdString
     }
+  }
   );
   return json(markers);
 }
