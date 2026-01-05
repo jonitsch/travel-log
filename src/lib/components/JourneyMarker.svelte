06ab340be55567d2e78bf25a60019b481ffe75e3
Jonathan <joni.tschanter@gmx.de>
2026-01-05 21:56:01 +0200

    Create Component Journey Marker

---
+++ src/lib/components/JourneyMarker.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+	import { Marker, Popup } from 'svelte-maplibre';
+	import { type LngLatLike } from 'maplibre-gl';
+
+	interface Props {
+		lngLat: LngLatLike;
+		popupText?: string | null;
+		height?: string;
+		width?: string;
+		color: string;
+		onclick?: () => void;
+		rest: any;
+	}
+	let {
+		lngLat,
+		popupText = null,
+		height = '3',
+		width = '3',
+		color,
+		onclick = () => {},
+		...rest
+	}: Props = $props();
+
+	const markerStyle = 'place-items-center rounded-full focus:outline-2 focus:outline-black';
+	const popupStyle = 'text-[1rem] px-3 py-0.4 rounded-md text-white opacity-95';
+</script>
+
+<Marker {lngLat} class={`h-${height} w-${width} ${markerStyle} ${color}`}>
+	{#if popupText}
+		<Popup anchor="bottom" offset={-15} closeOnClickOutside={false} closeButton={false} {...rest}>
+			<button class={`${popupStyle} ${color}`} onclick={() => onclick()}>
+				<text class="oxygen-regular">
+					{popupText}
+				</text>
+			</button>
+		</Popup>
+	{/if}
+</Marker>
