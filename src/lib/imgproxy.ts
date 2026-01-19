e9f6c132949291499a24bbcc94be83277483579d
jonitsch <83416807+jonitsch@users.noreply.github.com>
2026-01-19 19:50:34 +0100

    Add format parameter to getImgProxyURL() and improved parameter fallbacks

--- src/lib/imgproxy.ts
+++ src/lib/imgproxy.ts
@@ -1,9 +1,13 @@
-export async function getImgProxyURL(src: string, width?: number, height?: number): Promise<string> {
-    const params = new URLSearchParams({
-        src: src,
-        width: width ? Math.round(width)?.toString() : '',
-        height: height ? Math.round(height)?.toString() : ''
-    });
+export async function getImgProxyURL(
+    src: string,
+    width?: number,
+    height?: number,
+    format?: string): Promise<string> {
+    const params = new URLSearchParams();
+    params.append('src', src);
+    if (width) params.append('width', Math.round(width).toString());
+    if (height) params.append('height', Math.round(height).toString());
+    if (format) params.append('format', format);
 
     const response = await fetch(`/api/imgproxy?${params.toString()}`);
     let url = response.json();
