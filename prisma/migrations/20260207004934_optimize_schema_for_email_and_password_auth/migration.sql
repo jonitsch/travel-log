da64bbac9092bfdbdd54690ee35a8b815a018ab8
Jonathan <joni.tschanter@gmx.de>
2026-02-09 14:49:24 +0100

    Remove faulty migration from prisma/migrations

--- prisma/migrations/20260207004934_optimize_schema_for_email_and_password_auth/migration.sql
+++
@@ -1,8 +0,0 @@
--- AlterTable
-ALTER TABLE `user` MODIFY `username` VARCHAR(191) NULL;
-
--- CreateIndex
-CREATE INDEX `account_userId_idx` ON `account`(`userId`(191));
-
--- CreateIndex
-CREATE INDEX `session_userId_idx` ON `session`(`userId`(191));
