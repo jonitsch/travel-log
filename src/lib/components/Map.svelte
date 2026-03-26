779da3fd3fef7f6bde17f2c69d879b91934a316a
Jonathan <joni.tschanter@gmx.de>
2026-03-26 10:47:36 +0100

    Fix Incorrect calcOptimizedZoom() references

--- src/lib/components/Map.svelte
+++ src/lib/components/Map.svelte
@@ -5,7 +5,7 @@
 	import { global, type ViewMode } from '$lib/state.svelte';
 	import { onMount } from 'svelte';
 	import {
-		calcInitZoom,
+		calcOptimizedZoom,
 		defaultMapCenter,
 		switchToJourney,
 		switchToOverview
@@ -27,7 +27,7 @@
 	let bounds = $state<maplibregl.LngLatBoundsLike | undefined>(),
 		center = $state<maplibregl.LngLatLike | undefined>(defaultMapCenter);
 
-	let initialZoom = calcInitZoom(innerWidth.current ?? 0),
+	let initialZoom = calcOptimizedZoom(innerWidth.current ?? 0),
 		zoom = $state<number>(initialZoom);
 
 	let createJourneyModal = $state<CreateJourneyModal>();
